eec9cc1fc98a8115d85c1a89a28d5021024cfeeb
lanyi <lanyi@smart-dec.com>
2017-07-07 17:55:58 +0800

    fixed logic and build the base

--- src/machine/secondary_sort/secondary_sort.py
+++ src/machine/secondary_sort/secondary_sort.py
@@ -7,30 +7,22 @@ class SecondarySort:
         self.machine_id = machine_id
         self.queue = queue
 
-        self.i_infeed = queue #todo @lanyi: infeed io
-        self.i_outlet = queue #todo @lanyi: outlet io
-
-        self.c_infeed = queue #todo @lanyi: infeed io
-        self.c_outlet = queue #todo @lanyi: outlet io
-
-        self.shortcut_time = queue #todo @lanyi: shortcut time
-        self.machine_time = queue #todo @lanyi: machine time
-
         self.log_error = logger(log_name=f"error_{self.machine_id}")
 
-    def shortcut(self):
-        while True:
-            yield self.env.timeout(self.shortcut_time)
+        self.wait_queue = simpy.PriorityStore(self.env)
 
-    def machine(self):
+    def machine(self, time):
         while True:
-            yield self.env.timeout(self.machine_time)
+            yield self.env.timeout(time)
 
     def run(self):
         while True:
-            # todo @lanyi: get destination attr
-            package = yield self.queue.get()
+            package = yield self.wait_queue.get()
             package = package.item
-            if package.attr['type'] == "half":
-                self.env.process(self.shortcut())
-            self.env.process(self.machine())
+            destination = package.attr['dest_code']
+            process_time = package.attr['time']
+            #todo @lanyi will be replaced with a function
+            if destination in ['c1']:
+                #todo @lanyi shortcut function
+                pass
+            self.env.process(self.machine(process_time))
