940224307fd681a29a2d9ff6ea33fc5da93666a5
lanyi <lanyi@smart-dec.com>
2017-07-07 15:39:30 +0800

    add basic logic

--- src/machine/secondary_sort/secondary_sort.py
+++ src/machine/secondary_sort/secondary_sort.py
@@ -1,29 +1,36 @@
+import simpy
+
+
 class SecondarySort:
     def __init__(self, env, machine_id, queue, logger):
         self.env = env
         self.machine_id = machine_id
         self.queue = queue
 
-        self.log_record = logger(log_name=f"record_{self.machine_id}")
+        self.i_infeed = queue #todo @lanyi: infeed io
+        self.i_outlet = queue #todo @lanyi: outlet io
+
+        self.c_infeed = queue #todo @lanyi: infeed io
+        self.c_outlet = queue #todo @lanyi: outlet io
+
+        self.shortcut_time = queue #todo @lanyi: shortcut time
+        self.machine_time = queue #todo @lanyi: machine time
+
         self.log_error = logger(log_name=f"error_{self.machine_id}")
 
     def shortcut(self):
         while True:
-            self.env.timeout(1)
-            pass
-
+            yield self.env.timeout(self.shortcut_time)
 
     def machine(self):
         while True:
-            package = yield self.queue.get()
-            package = package.item
-            destination = package.attr['destination_port']
+            yield self.env.timeout(self.machine_time)
 
     def run(self):
         while True:
+            # todo @lanyi: get destination attr
             package = yield self.queue.get()
             package = package.item
             if package.attr['type'] == "half":
-                self.shortcut()
-            else:
-                pass
+                self.env.process(self.shortcut())
+            self.env.process(self.machine())
