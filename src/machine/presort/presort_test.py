7b7d9370a306030743399374aab7b775103288c7
chois lu <chois_lu@outlook.com>
2017-07-14 18:33:07 +0800

    优化了test代码，但资源请求好像还存在一点问题

--- src/machine/presort/presort_test.py
+++ src/machine/presort/presort_test.py
@@ -22,6 +22,7 @@
 import simpy
 import random
 from src.machine.presort import Presort
+from src.vehicles.items import Package
 
 
 RANDOM_SEED = 42
@@ -37,8 +38,8 @@ random.seed(RANDOM_SEED)
 
 # 生成单个入口包裹到达的时间间隔
 def package_time_interval():
-    return random.uniform(40, 60)
-
+    # return random.uniform(40, 60)
+    return random.randint(2, 10)
 
 # 生成单个入口包裹队列的长度
 def package_num():
@@ -47,8 +48,8 @@ def package_num():
 
 # 生成一个入口到一个出口的处理时间
 def process_time():
-    return random.uniform(40, 200)
-
+    # return random.uniform(40, 200)
+    return 10 #random.randint(10, 30)
 
 # 生成出口编号
 def generate_dest_id():
@@ -60,64 +61,42 @@ def generate_dest_id():
         dest_list += DEST_ID[mid]
     return list(set(dest_list))
 
-
-class PackagePresort(object):
-    """
-    测试用包裹类
-    """
-
-    def __init__(self, package_id, path, generate_time):
-        self.package_id = package_id
-        self.path = path[:]
-        self.plan_path = path[:]
-        self.time_list = [generate_time]
-
-
-# 生成所有的输入包裹
-def generate_package():
-    for mid in MACHINE_ID:
-        interval = package_time_interval()
+# 生成货物的process
+def generate_package(env, mid):
+    package_queue = INPUT_QUEUE_DICT[mid]
+    interval = package_time_interval()
+    for i in range(package_num()):
+        yield env.timeout(interval)
         dest_id = random.choice(DEST_ID[mid])
-        path = [mid, dest_id]
-        ALL_PACKAGES[mid] = []
-        for i in range(package_num()):
-            ALL_PACKAGES[mid].append(
-                PackagePresort(f'{mid}-{i+1}', path, interval * i))
-
-
-# 生成输入队列
-def generate_queue(env, machine_id):
-    package_queue = simpy.PriorityStore(env)
-    for i in range(len(ALL_PACKAGES[machine_id])):
-        item = ALL_PACKAGES[machine_id].pop(0)
-        package_queue.put(
-            simpy.PriorityItem(priority=item.time_list[0], item=item))
-    INPUT_QUEUE_DICT[machine_id] = package_queue
+        path = (mid, dest_id)
+        item = Package(env, None, f'{mid}-{i+1}', path)
+        item.time_records.append((path[0], env.now)) # 货物到达机器的时间
+        package_queue.put(item)
 
 
 if __name__ == '__main__':
     print("start")
     dest_id_list = generate_dest_id()
-    generate_package()
     machine_dict = {}
     env = simpy.Environment()
-    for did in dest_id_list:
-        OUTPUT_QUEUE_DICT[did] = simpy.PriorityStore(env)
+
+    # 生成输入和输出队列
     for mid in MACHINE_ID:
-        generate_queue(env, mid)
+        INPUT_QUEUE_DICT[mid] = simpy.Store(env)
+    for did in dest_id_list:
+        OUTPUT_QUEUE_DICT[did] = simpy.Store(env)
+
+    for mid in MACHINE_ID:
+        # 生成货物
+        env.process(generate_package(env, mid))
+
+        # 生成机器
         input_queue = INPUT_QUEUE_DICT[mid]
-        time = process_time()
+        time = 10 #process_time()
         presort_machine = Presort(env, mid, time, WORKER_NUM, input_queue,
                                   OUTPUT_QUEUE_DICT)
         machine_dict[mid] = presort_machine
         env.process(presort_machine.run())
-    env.run()
-    for _, next_queue in OUTPUT_QUEUE_DICT.items():
-        for item in next_queue.items:
-            package = item.item
-            print(
-                f'Package {package.package_id} go to {package.plan_path[0]} at {package.time_list[0]}')
-            print(
-                f'Package {package.package_id} go to {package.plan_path[1]} at {package.time_list[1]}')
-    print("End")
 
+    env.run()
+    print("End.")
