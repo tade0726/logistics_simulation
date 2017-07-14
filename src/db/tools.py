5bcabafce664f0d6ec6f63f9b1bdb8399f31df96
teddyz <zp913913@gmail.com>
2017-07-14 16:38:16 +0800

    - add testing

--- src/db/tools.py
+++ src/db/tools.py
@@ -16,7 +16,6 @@ from sqlalchemy import create_engine
 from os.path import realpath, join, split
 from collections import defaultdict
 
-
 class MySQLConfig:
 
     HOST = ""
@@ -33,63 +32,67 @@ class MySQLConfig:
 class SaveConfig:
 
     DATA_DIR = join( split(split(split(realpath(__file__))[0])[0])[0], 'data')
-    DATA_FILE = 'tables.pkl'
-    DATA_PATH  = join(DATA_DIR, DATA_FILE)
+    DATA_FILE = 'tables.csv'
+    DATA_PATH = join(DATA_DIR, DATA_FILE)
 
 
 def load_from_local(table_name: str):
-
-    try:
-        table = pd.read_csv(join(SaveConfig.DATA_DIR, f"{table_name}.csv"))
-    except Exception as exc:
-        print(exc)
-        table = None
-
+    """本地读取数据，数据格式为csv"""
+    table = pd.read_csv(join(SaveConfig.DATA_DIR, f"{table_name}.csv"))
     return table
 
 
 def load_from_mysql(table_name: str):
-    try:
-        table = pd.read_sql_table(con=MySQLConfig.engine, table_name=f"{table_name}")
-    except Exception as exc:
-        print(exc)
-        table = None
-
+    """读取远程mysql数据表"""
+    table = pd.read_sql_table(con=MySQLConfig.engine, table_name=f"{table_name}")
     return table
 
 
 def get_trucks(is_test: bool=False, is_local: bool=False):
-
     """
-    :param is_test:
-    :param is_local:
-    :return: truck dict
-
-    des:
-    get data from local file or mysql
+    返回货车数据，字典形式：
+        key 为 （货车编号， 到达时间，货车货物路径类型（LL／LA..））
+        value 为 一个货车的 packages 数据表
     """
     table_name = "i_od_parcel_landside"
-
     if is_local:
-        land_list = load_from_local(table_name)
+        table = load_from_local(table_name)
     else:
-        land_list = load_from_mysql(table_name)
-
+        table = load_from_mysql(table_name)
     if is_test:
-        land_list = land_list[: 1000]
+        table = table.head(1000)
+    # add path_type: LL/LA/AL/AA
+    table['path_type'] = table['origin_type'] + table['dest_type']
+    # 'plate_num' 是货车／飞机／的编号
+    return dict(list(table.groupby(['plate_num', 'arrive_time', 'path_type'])))
 
-    # add truck type: LL/LA/AL/AA
-    land_list['truck_type'] = land_list['origin_type'] + land_list['dest_type']
-    truck_dict = dict(list(land_list.groupby(['plate_num', ' arrive_time', 'truck_type'])))
-    return truck_dict
 
-
-def get_ulds(is_local: bool=False):
-    pass
+def get_ulds(is_test: bool=False, is_local: bool=False):
+    """
+    返回uld数据，字典形式：
+        key 为 （货车编号， 到达时间，货车货物路径类型（LL／LA..））
+        value 为 一个uld的 packages 数据表
+    """
+    table_name = "i_od_parcel_airside"
+    if is_local:
+        table = load_from_local(table_name)
+    else:
+        table = load_from_mysql(table_name)
+    if is_test:
+        table = table.head(1000)
+    # add path_type: LL/LA/AL/AA
+    table['path_type'] = table['origin_type'] + table['dest_type']
+    # 'plate_num' 是货车／飞机／的编号
+    return dict(list(table.groupby(['uld_num', 'arrive_time', 'path_type'])))
 
 
 def get_unload_setting(is_local: bool=False):
-    """返回 reload port 和 truck 类型（LL， LA， AL ，AA） 的映射"""
+    """
+    返回字典形式：
+        reload port 和 truck 类型（LL， LA， AL ，AA） 的映射
+    examples:
+        {'r1_1': ['LL'], 'r3_1': ['LL', 'LA']}
+    """
 
     table_name = "i_unload_setting"
 
@@ -106,64 +109,67 @@ def get_unload_setting(is_local: bool=False):
         table_dict[row['equipment_port']].append(row['truck_type'])
     return table_dict
 
-# todo
+
 def get_reload_setting(is_local: bool=False):
+    """"""
     pass
 
+def get_resource_limit(is_local: bool=False):
+    """返回资源表，包含了单个资源处理时间"""
+    table_name1 = "i_resource_limit"
+    table_name2 = "i_equipment_resource"
+    table_name3 = "i_equipment_io"
 
-def get_resource_table(is_local: bool=False):
-    """
-    :param is_local:
-    :return: dict of resource {'equipment_in_port': dataframe.series}
-    """
-    resource_limit = "i_resource_limit"
-    resource_equipment = "i_equipment_resource"
-
-    # get tables
     if is_local:
-        resource_limit_table = load_from_local(resource_limit)
-        resource_equipment_table = load_from_local(resource_equipment)
-
+        table1 = load_from_local(table_name1)
+        table2 = load_from_local(table_name2)
+        table3 = load_from_local(table_name3)
     else:
-        resource_limit_table = load_from_mysql(resource_limit)
-        resource_equipment_table = load_from_mysql(resource_equipment)
+        table1 = load_from_mysql(table_name1)
+        table2 = load_from_mysql(table_name2)
+        table3 = load_from_mysql(table_name3)
 
-    # merge table
+    table = table1.merge(table2[['resource_id', 'equipment_id']], how='left', on='resource_id')\
+                .merge(table3[['equipment_id', 'process_time']], how='left', on='equipment_id')
 
-    resource_table = resource_equipment_table.merge(
-        resource_limit_table,
-        how='left',
-        on='resource_id',)
+    return table
 
-    resource_table_dict = dict(list(resource_table.groupby('equipment_in_port')))
-
-    return resource_table_dict
-
-# todo
 def get_pipeline(is_local: bool=False):
-    pass
 
-def get_machine_parameters(is_local: bool=False):
-    """
-    :param is_local:
-    :return: dict of dict of parameters
+    """返回队列的表， 包含了每个队列对应的功能区域"""
 
-    des: getting machine var
-    """
-    table_name = "i_equipment_parameter"
+    table_name = "i_queue_io"
 
     if is_local:
-        parameter_list = load_from_local(table_name)
+        table = load_from_local(table_name)
     else:
-        parameter_list = load_from_mysql(table_name)
+        table = load_from_mysql(table_name)
 
-    parameter_dict = \
-        {key: dict(list(val.groupby('parameter_id'))) for key, val in list(parameter_list.groupby('equipment_id'))}
+    machine_dict = \
+    {'LM': 'presort',
+     'LS': 'secondary_sort',
+     'SE': 'security',
+     'AM': 'presort',
+     'AS': 'secondary_sort',
+     'MS': 'small_sort',}
 
-    return parameter_dict
+    table = table[['equipment_port_last', 'equipment_port_next', 'sorter_zone']]
+    table['machine_name'] = table['sorter_zone'].apply(lambda x: x[:2]).replace(machine_dict)
+    return table
 
+
+def get_queue_io(is_local: bool):
+    """返回 io 对: [(r1_1,  m1_1), (r1_3, m2_3), ]"""
+    table = get_pipeline(is_local)
+    io_list = []
+    for _, row in table.iterrows():
+        io_list.append((row['equipment_port_last'], row['equipment_port_next']))
+
+    return io_list
 
 
 if __name__ == '__main__':
-    test = get_unload_setting(is_local=True)
-    print(test)
+    test1 = get_unload_setting(is_local=True)
+    test2 = get_resource_limit(is_local=True)
+    test3 = get_pipeline(is_local=True)
+    test4 = get_queue_io(is_local=True)
