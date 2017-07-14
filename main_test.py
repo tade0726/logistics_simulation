5e144ef6cf513ae323bee41c462de761c5be941e
teddyz <zp913913@gmail.com>
2017-07-14 09:21:58 +0800

    - a real main.py

---
+++ main_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Author: Ted
+Date: 2017-07-13
+des:
+    main.py
+"""
+
+
+import simpy
+
+
+# todo:
+# - need resource ／ need link to port and resource
+# - need pipeline / need pipeline id
+# - need parameters / empty
