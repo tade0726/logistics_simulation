a1899f8eb0490bd8a49de656a327190cc5300218
teddyz <zp913913@gmail.com>
2017-07-16 17:35:41 +0800

    - add reading reload setting

--- src/db/tools.py
+++ src/db/tools.py
@@ -89,7 +89,7 @@ def get_ulds(is_test: bool=False, is_local: bool=False):
 def get_unload_setting(is_local: bool=False):
     """
     返回字典形式：
-        reload port 和 truck 类型（LL， LA， AL ，AA） 的映射
+        unload port 和 truck 类型（LL， LA， AL ，AA） 的映射
     examples:
         {'r1_1': ['LL'], 'r3_1': ['LL', 'LA']}
     """
@@ -111,8 +111,24 @@ def get_unload_setting(is_local: bool=False):
 
 
 def get_reload_setting(is_local: bool=False):
-    """"""
-    pass
+    """
+    返回字典形式：
+        dest_code 和 reload port 类型的映射
+    examples:
+        { "571J": [c1_1, ], }
+    """
+
+    table_name = "i_reload_setting"
+
+    if is_local:
+        table = load_from_local(table_name)
+    else:
+        table = load_from_mysql(table_name)
+
+    table_dict = defaultdict(list)
+    for _, row in table.iterrows():
+        table_dict[row['dest_code']].append(row['equipment_port'])
+    return table_dict
 
 
 def get_resource_limit(is_local: bool=False):
@@ -136,9 +152,9 @@ def get_resource_limit(is_local: bool=False):
     return table
 
 
-def get_pipeline(is_local: bool=False):
+def get_pipelines(is_local: bool=False):
 
-    """返回队列的表， 包含了每个队列对应的功能区域"""
+    """返回队列的表， 包含了每个队列对应的功能区域和传送时间"""
 
     table_name = "i_queue_io"
 
@@ -155,14 +171,14 @@ def get_pipeline(is_local: bool=False):
      'AS': 'secondary_sort',
      'MS': 'small_sort',}
 
-    table = table[['equipment_port_last', 'equipment_port_next', 'sorter_zone']]
-    table['machine_name'] = table['sorter_zone'].apply(lambda x: x[:2]).replace(machine_dict)
+    table = table[['equipment_port_last', 'equipment_port_next', 'sorter_zone', 'process_time', 'queue_id']]
+    table['machine_type'] = table['sorter_zone'].apply(lambda x: x[:2]).replace(machine_dict)
     return table
 
 
 def get_queue_io(is_local: bool):
     """返回 io 对: [(r1_1,  m1_1), (r1_3, m2_3), ]"""
-    table = get_pipeline(is_local)
+    table = get_pipelines(is_local)
     io_list = []
     for _, row in table.iterrows():
         io_list.append((row['equipment_port_last'], row['equipment_port_next']))
@@ -174,12 +190,12 @@ if __name__ == '__main__':
 
     test1 = get_unload_setting(is_local=True)
     test2 = get_resource_limit(is_local=True)
-    test3 = get_pipeline(is_local=True)
+    test3 = get_pipelines(is_local=True)
     test4 = get_queue_io(is_local=True)
+    test5 = get_reload_setting(is_local=True)
 
+    # this a test
     with open('tools.py.txt', 'at') as file:
-        for obj in [test1, test2, test3, test4]:
+        for obj in [test1, test2, test3, test4, test5]:
             file.writelines(obj.__str__() + "\n")
             file.writelines("\n" + "=" * 60 + "\n")
-
-
