dbe4d092ddb7c27da8cb45b3bd06bf7ef6592443
teddyz <zp913913@gmail.coom>
2017-07-17 15:54:12 +0800

    - 添加数据点

--- src/machine/unload/unload_test.py
+++ src/machine/unload/unload_test.py
@@ -1,6 +1,7 @@
 import simpy
 import random
-
+from collections import defaultdict
+import pickle
 
 class Truck:
 
@@ -10,10 +11,10 @@ class Truck:
 
 
 def truck_controllers(env: simpy.Environment, truck_q: simpy.Store):
-    for truck_id in range(100):
-        truck_q.put(Truck(truck_id=truck_id))
-        TIME = 5
-        yield env.timeout(TIME)
+    for i in range(100):
+        truck_q.put(Truck(truck_id=f"truck_{i}"))
+        wait_time = 5
+        yield env.timeout(wait_time)
 
 
 class Unload:
@@ -30,12 +31,25 @@ class Unload:
         self.packages_processed = dict()
         self.process_time = 3.6
 
+        # store data
+        self.package_records = defaultdict(dict)
+        self.truck_records = defaultdict(dict)
+
     def processing(self, idx, package):
 
         with self.resource.request() as req:
             yield req
-            yield self.env.timeout(self.process_time)
+
             print(f"{package} start processing unload_{self.machine_id} at {self.env.now}")
+            self.package_records[package]["start_process"] = self.env.now
+
+            yield self.env.timeout(self.process_time)
+
+            print(f"{package} end processing unload_{self.machine_id} at {self.env.now}")
+            self.package_records[package]["end_process"] = self.env.now
+
+            self.package_records[package]["machine_id"] = self.machine_id
+            self.package_records[package]["package_id"] = package
             self.next_q.put(package)
             self.packages_processed[idx].succeed()
 
@@ -43,28 +57,51 @@ class Unload:
         while True:
 
             truck = yield self.truck_q.get()
+            truck_arrive_time = self.env.now
+
+            print(f"truck_{truck.truck_id} enter unload_{self.machine_id} at {truck_arrive_time}")
+            self.truck_records[truck.truck_id]["enter_machine"] = truck_arrive_time
 
             for idx, package in enumerate(truck.packages):
-                print(f"{package} enter unload_{self.machine_id} at {self.env.now}")
+                print(f"{package} start waiting unload_{self.machine_id} at {truck_arrive_time}")
+                self.package_records[package]["start_wait"] = truck_arrive_time
+
                 self.packages_processed[idx] = self.env.event()
                 self.env.process(self.processing(idx, package))
 
             yield self.env.all_of(self.packages_processed.values())
             # truck convert time
+            print(f"truck_{truck.truck_id} leave unload_{self.machine_id} at {self.env.now}")
+            self.truck_records[truck.truck_id]["leave_machine"] = self.env.now
+            self.truck_records[truck.truck_id]["machine_id"] = self.machine_id
+            self.truck_records[truck.truck_id]["truck_id"] = truck.truck_id
             yield self.env.timeout(self.truck_convert_time)
             self.truck_counts += 1
 
 
-if __name__  == "__main__":
+if __name__ == "__main__":
 
     env = simpy.Environment()
     truck_q = simpy.Store(env)
     next_q = simpy.Store(env)
 
-    unload_machines = [ Unload(env, machine_id, truck_q, next_q) for machine_id in range(2)]
+    unload_machines = [Unload(env, f"unload_{i}", truck_q, next_q) for i in range(2)]
 
     for machine in unload_machines:
         env.process(machine.run())
     env.process(truck_controllers(env, truck_q))
 
     env.run()
+
+    # loading data
+    truck_records = dict()
+    package_records = dict()
+    for machine in unload_machines:
+        truck_records.update(machine.truck_records)
+        package_records.update(machine.package_records)
+
+    with open("truck_records.dict", "wb") as file:
+        pickle.dump(truck_records, file)
+
+    with open("package_records.dict", "wb") as file:
+        pickle.dump(package_records, file)
