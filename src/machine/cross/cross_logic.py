bb7b42d7b261795be66dd086015f8175396bde11
chois lu <chois_lu@outlook.com>
2017-07-19 21:39:01 +0800

    添加逻辑测试模组，更新cross cross测试更新可测

---
+++ src/machine/cross/cross_logic.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+
+from src.machine.cross.test.config import Config
+from src.vehicles import Pipeline
+from src.machine.cross.test.logic_test import LogicTest
+from simpy import Environment
+from src.machine.cross import Cross
+
+
+class CrossSimConfig(Config):
+    # RANDOM_SEED = 57
+    # NUM_PACKAGES = 100
+    # INTERVAL_TIME = 10
+    TYPE_PIP_LINE = Pipeline
+    # ==========================测试机配置参数===================================
+    # 本次杭州仿真模为一个入口队列一个机器
+    # 测试机器ID列表
+    ID_TEST_MACHINE = ['test1', 'test2']
+    # 测试机器出端口id列表
+    ID_NEXT_MACHINE = ['next_1']
+    # 测试机器资源
+    TEST_MACHINE_RESOURCE = 0  # 如果测试机器内部无资源调用，设置为0，否则设置资源数(如人力)
+    # 测试机单资源处理时延
+    PROCESS_TIME = None  # 如果测试机器没有处理货物延时，设置为None，否则设置为对应延时
+
+
+if __name__ == '__main__':
+    env = Environment()
+    Pipeline(env=env, delay_time=0, pipeline_id=('id1', 'id2'))
+    t1 = LogicTest(env, CrossSimConfig)
+    t1.generator()
+    env.process(t1.packages_generator())
+    for tid in t1.config.ID_TEST_MACHINE:
+        input_pip_line = t1.get_input_pip_line(tid)
+        Cross(env=env,
+              machine_id=tid,
+              equipment_id=tid,
+              input_pip_line=input_pip_line,
+              pipelines_dict=t1.pipline,
+              resource_dict=t1.get_reusource_dic())
+
+    env.run(until=100)
---
+++ src/machine/cross/test/config.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+from simpy import Store
+
+
+__all__ = ['Config']
+
+
+class Config(object):
+
+    # 随机种子配置
+    RANDOM_SEED = 42
+    # 机器树的层数
+    NODE_LAYERS = 3
+    # package生成器配置信息
+    NUM_PACKAGES = 10   # 生成package数量/个
+    # 包裹生成间隔
+    INTERVAL_TIME = 1
+    # 包裹传送带类型：pipline 类/simpy.Store/simpy.PriorityStore
+    TYPE_PIP_LINE = Store
+    ID_LAST_MACHINE = ['package_generator']
+    # 测试机器id表
+    ID_TEST_MACHINE = ['test_machine_1', 'test_machine_2', 'test_machine_3', 'test_machine_4']
+    # 测试机器入口数量
+    TEST_MACHINE_INPUT_PORT_NUM = ['port_1', 'port_2']
+    # 测试机器资源
+    TEST_MACHINE_RESOURCE = 0
+    # 根据测试机器后面可能去的机器ID配置, 其实为侧机器出口数量
+    ID_NEXT_MACHINE = ['next_machine_1', 'next_machine_2', 'next_machine_3', 'next_machine_4']
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def init_app(app):
+        pass
