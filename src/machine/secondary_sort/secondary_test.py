2457b3469234054176d749707e9e2d5859cb818a
lanyi <lanyi@smart-dec.com>
2017-07-19 16:27:09 +0800

    重构secondary_sort_test，添加next_pipeline到secondary_sort

--- src/machine/secondary_sort/secondary_test.py
+++ src/machine/secondary_sort/secondary_test.py
@@ -23,33 +23,18 @@
 
 import simpy
 import random
-from src.machine.secondary_sort import SecondarySort
 from src.vehicles.items import Package
 
 
 RANDOM_SEED = 42
 CART_NUM = 300
-MACHINE_ID = ["i2-1", "i2-2", "i2-3"]
-DEST_NUM = 3
+MACHINE_ID = ["i2-1"]
+DEST_NUM = 50
 DEST_ID = {}
 ALL_PACKAGES = {}
 INPUT_QUEUE_DICT = {}
 OUTPUT_QUEUE_DICT = {}
-
-
-# 生成单个入口包裹到达的时间间隔
-def package_time_interval():
-    return random.uniform(40, 60)
-
-
-# 生成单个入口包裹队列的长度
-def package_num():
-    return random.randint(5, 10)
-
-
-# 生成一个入口到一个出口的处理时间
-def process_time():
-    return random.uniform(40, 200)
+random.seed(RANDOM_SEED)
 
 
 # 生成出口编号
@@ -58,89 +43,108 @@ def generate_dest_id():
     for mid in MACHINE_ID:
         DEST_ID[mid] = []
         for i in range(DEST_NUM):
-            DEST_ID[mid].append(mid.replace("i", "c")[0:3] + str(i + 1))
+            DEST_ID[mid].append(mid.replace("i", "c")[0:3] + str(i))
         dest_list += DEST_ID[mid]
-    return list(set(dest_list))
+    return dest_list
 
 
-class PackageSecondary(object):
-    """
-    测试用包裹类
-    """
-
-    def __init__(self, package_id, path, generate_time, env):
-        self.package_id = package_id
-        self.path = path[:]
-        self.plan_path = path[:]
-        self.time_list = [generate_time]
-        self.time_records = []
-        self.env = env
-
-    def ret_pop_mark(self):
-        """返回下一个pipeline id: (now_loc, next_loc)， 删去第一个节点，记录当前的时间点"""
-
-        if len(self.path) >= 2:
-            now_loc, next_loc = self.path[0: 2]
-        # 当 package 去到 reload（终分拣）， 终分拣的队列 id 只有一个值
-        elif len(self.path) == 1:
-            now_loc, next_loc = self.path[-1], None
-        else:
-            raise ValueError('The path have been empty!')
-        # remove the now_loc
-        pop_loc = self.path.pop(0)
-        self.time_records.append((pop_loc, self.env.now))
-        return now_loc, next_loc
-
-
-# 生成所有的输入包裹
-def generate_package():
-    for mid in MACHINE_ID:
-        interval = package_time_interval()
+# 生成货物的process
+def generate_package(env, mid):
+    package_queue = INPUT_QUEUE_DICT[mid]
+    interval = random.randint(2, 10)
+    for i in range(random.randint(100, 200)):
+        yield env.timeout(interval)
         dest_id = random.choice(DEST_ID[mid])
-        path = [mid, dest_id]
-        ALL_PACKAGES[mid] = []
-        for i in range(package_num()):
-            ALL_PACKAGES[mid].append(
-                PackageSecondary(f'package:{mid}-{i+1}', path, interval * i, simpy.Environment()))
+        path = (mid, dest_id)
+        item = Package(env, None, f'{mid}-{i+1}', path)
+        item.next_pipeline = path
+        item.time_records.append((path[0], env.now))
+        package_queue.put(item)
 
 
-# 生成输入队列
-def generate_queue(env, machine_id):
-    package_queue = simpy.PriorityStore(env)
-    for i in range(len(ALL_PACKAGES[machine_id])):
-        item = ALL_PACKAGES[machine_id].pop(0)
-        package_queue.put(
-            simpy.PriorityItem(priority=item.time_list[0], item=item))
-    INPUT_QUEUE_DICT[machine_id] = package_queue
+class SecondarySort(object):
+    """
+    预分拣机器的仿真
+    """
+    def __init__(self,
+                 env: simpy.Environment,
+                 machine_id: str,
+                 process_time: float,
+                 cart_num: int,
+                 input_queue,
+                 output_queue_dict
+                 ):
+        """
+
+        """
+        self.env = env
+        self.machine_id = machine_id
+        self.process_time = process_time  # 处理时间
+        self.cart_num = cart_num  # 资源（工人）数量
+
+        # 入口队列
+        self.last_queue = input_queue
+
+        # 出口队列
+        self.next_queue = output_queue_dict
+
+        # 人力资源
+        self.carts = simpy.Resource(self.env, self.cart_num)
+
+        # 检查队列是否为空， TODO 为了跑测试程序，将第二行注释，正式程序需去掉注释
+        self.empty = self.env.event()
+        # self.env.process(self.empty_queue())
+
+        # 等待队列包裹数
+        self.package_counts = 0
+
+    def secondary_machine(self, package: Package):
+        with self.carts.request() as req:
+            yield req
+            start_time = package.start_serve()
+            now_loc, next_loc = package.next_pipeline
+            yield self.env.timeout(self.process_time)
+            self.next_queue[next_loc].put(package)
+            package.time_records.append((next_loc, self.env.now))
+            end_time = package.end_serve()
+            print(
+                f'Package {package.item_id} arrive in {now_loc} at '
+                f'{package.package_record["start_serve"]} process {self.process_time} '
+                f'going to {next_loc} end {package.package_record["end_serve"]}'
+            )
+
+    def run(self):
+        while True:
+            package = yield self.last_queue.get()
+            self.package_counts += 1
+            self.env.process(self.secondary_machine(package))
 
 
-def secondary_sim():
+if __name__ == '__main__':
+    print("start")
+
     dest_id_list = generate_dest_id()
-    generate_package()
+
     machine_dict = {}
     env = simpy.Environment()
+
+    # 生成输入和输出队列
     for mid in MACHINE_ID:
         INPUT_QUEUE_DICT[mid] = simpy.Store(env)
     for did in dest_id_list:
         OUTPUT_QUEUE_DICT[did] = simpy.Store(env)
 
     for mid in MACHINE_ID:
-        generate_queue(env, mid)
+        cou = 1
+        count = str(cou)
+        env.process(generate_package(env, mid))
         input_queue = INPUT_QUEUE_DICT[mid]
-        secondary_machine = SecondarySort(env=env,
-                                          machine_id=mid,
-                                          process_time=process_time(),
-                                          cart_num=CART_NUM,
-                                          input_queue=input_queue,
-                                          output_queue=OUTPUT_QUEUE_DICT)
-        machine_dict[mid] = secondary_machine
-        env.process(secondary_machine.secondary_machine(ALL_PACKAGES))
+        time = 10
+        secondary_sort_machine = SecondarySort(env, count, time, CART_NUM, input_queue,
+                                  OUTPUT_QUEUE_DICT)
+        machine_dict[mid] = secondary_sort_machine
+        env.process(secondary_sort_machine.run())
+        cou += 1
+
     env.run()
-
-
-if __name__ == '__main__':
-    print("start")
-    # secondary_sim()
-    generate_package()
-    print(ALL_PACKAGES)
-    print("End")
+    print("End.")
