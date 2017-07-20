9ff4df28027cc54d211aedb30d167da0e98246fa
lanyi <lanyi@smart-dec.com>
2017-07-20 10:38:32 +0800

    添加Secondary Sort

--- src/machine/secondary_sort/secondary_test.py
+++ src/machine/secondary_sort/secondary_test.py
@@ -23,7 +23,10 @@
 
 import simpy
 import random
+from src.db import *
 from src.vehicles.items import Package
+from src.machine.secondary_sort import SecondarySort
+from src.vehicles import Pipeline
 
 
 RANDOM_SEED = 42
@@ -56,68 +59,10 @@ def generate_package(env, mid):
         yield env.timeout(interval)
         dest_id = random.choice(DEST_ID[mid])
         path = (mid, dest_id)
-        item = Package(env, None, f'{mid}-{i+1}', path)
+        item = Package(env, attr=None, item_id=f'{mid}-{i+1}', path=path)
         item.next_pipeline = path
-        item.time_records.append((path[0], env.now))
+        item.pipeline_data.append((path[0], env.now))
         package_queue.put(item)
-
-
-class SecondarySort(object):
-    """
-    预分拣机器的仿真
-    """
-    def __init__(self,
-                 env: simpy.Environment,
-                 machine_id: str,
-                 process_time: float,
-                 cart_num: int,
-                 input_queue,
-                 output_queue_dict
-                 ):
-        """
-
-        """
-        self.env = env
-        self.machine_id = machine_id
-        self.process_time = process_time  # 处理时间
-        self.cart_num = cart_num  # 资源（工人）数量
-
-        # 入口队列
-        self.last_queue = input_queue
-
-        # 出口队列
-        self.next_queue = output_queue_dict
-
-        # 人力资源
-        self.carts = simpy.Resource(self.env, self.cart_num)
-
-        # 检查队列是否为空， TODO 为了跑测试程序，将第二行注释，正式程序需去掉注释
-        self.empty = self.env.event()
-        # self.env.process(self.empty_queue())
-
-        # 等待队列包裹数
-        self.package_counts = 0
-
-    def secondary_machine(self, package: Package):
-        with self.carts.request() as req:
-            yield req
-            start_time = package.start_serve()
-            now_loc, next_loc = package.next_pipeline
-            yield self.env.timeout(self.process_time)
-            self.next_queue[next_loc].put(package)
-            package.time_records.append((next_loc, self.env.now))
-            end_time = package.end_serve()
-            print(
-                f'Package {package.item_id} arrive in {now_loc} at '
-                f'{package.package_record["start_serve"]} process {self.process_time} '
-                f'going to {next_loc} end {package.package_record["end_serve"]}'
-            )
-
-    def run(self):
-        while True:
-            package = yield self.last_queue.get()
-            self.package_counts += 1
-            self.env.process(self.secondary_machine(package))
 
 
 if __name__ == '__main__':
@@ -134,14 +79,30 @@ if __name__ == '__main__':
     for did in dest_id_list:
         OUTPUT_QUEUE_DICT[did] = simpy.Store(env)
 
+    # 生成pipeline
+    pipelines_table = get_pipelines(is_filter=False)
+    pipelines_dict = dict()
+    for _, row in pipelines_table.iterrows():
+        machine_type = row['machine_type']
+        queue_id = row['queue_id']
+        delay_time = row['process_time']
+        pipeline_id = row['equipment_port_last'], row['equipment_port_next']
+        pipelines_dict[pipeline_id] = Pipeline(env, delay_time, pipeline_id,
+                                               queue_id, machine_type)
+
     for mid in MACHINE_ID:
         cou = 1
         count = str(cou)
         env.process(generate_package(env, mid))
         input_queue = INPUT_QUEUE_DICT[mid]
         time = 10
-        secondary_sort_machine = SecondarySort(env, count, time, CART_NUM, input_queue,
-                                  OUTPUT_QUEUE_DICT)
+        secondary_sort_machine = SecondarySort(env,
+                                               count,
+                                               time,
+                                               CART_NUM,
+                                               pipelines_dict=pipelines_dict,
+                                               input_queue=input_queue,
+                                               output_queue=OUTPUT_QUEUE_DICT)
         machine_dict[mid] = secondary_sort_machine
         env.process(secondary_sort_machine.run())
         cou += 1
