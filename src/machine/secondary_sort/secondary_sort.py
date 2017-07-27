344814ccb6dc3b32f81b3bc57ff6a7e6726aba51
teddyz <zp913913@gmail.coom>
2017-07-27 12:13:53 +0800

    - move secondary function in pipelineRes

--- src/machine/secondary_sort/secondary_sort.py
+++ src/machine/secondary_sort/secondary_sort.py
@@ -33,45 +33,22 @@ class SecondarySort(object):
                  env: simpy.Environment(),
                  machine_id: tuple,
                  pipelines_dict: dict,
-                 equipment_process_time_dict:dict,
                  ):
 
         self.env = env
         self.machine_id = machine_id
         self.pipelines_dict = pipelines_dict
-        self.equipment_process_time_dict = equipment_process_time_dict
         self._set_machine()
 
     def _set_machine(self):
         """
         """
         self.equipment_id = self.machine_id[1]  # pipeline id last value, for other machines
-        self.process_time = self.equipment_process_time_dict[self.equipment_id]
         self.input_pip_line = self.pipelines_dict[self.machine_id]
-
-    def process_package(self, item: Package):
-        # package start for process
-        item.insert_data(
-            PackageRecord(
-                equipment_id=self.equipment_id,
-                package_id=item.item_id,
-                time_stamp=self.env.now,
-                action="start", ))
-
-        yield self.env.timeout(self.process_time)
-
-        # package end for process
-        item.insert_data(
-            PackageRecord(
-                equipment_id=self.equipment_id,
-                package_id=item.item_id,
-                time_stamp=self.env.now,
-                action="end", ))
-
-        next_pipeline = item.next_pipeline
-        self.pipelines_dict[next_pipeline].put(item)
 
     def run(self):
         while True:
             package = yield self.input_pip_line.get()
-            self.env.process(self.process_package(item=package))
+            next_pipeline = package.next_pipeline
+            self.pipelines_dict[next_pipeline].put(package)
+
