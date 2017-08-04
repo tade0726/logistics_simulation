fa29d41e12df2ed437644132efd82d96ee4f6165
chois lu <chois_lu@outlook.com>
2017-08-04 16:56:54 +0800

    更新路侧R__init__脚本

--- tk_fram/fram_r/__init__.py
+++ tk_fram/fram_r/__init__.py
@@ -9,12 +9,18 @@ root = Tk()
 
 def create_r_frame():
     """"""
+    #  =======================添加主视图====================
+    root = Tk()
+    #  =======================配置主视图尺寸=================
     root.geometry(f"{FRAME_WIDTH}x{FRAME_HEIGHT}+0+0")
     root.title('杭州分拣中心仿真程序')
     # root.minsize(FRAME_WIDTH, FRAME_HEIGHT)
     # root.maxsize(FRAME_WIDTH, FRAME_HEIGHT)
+    #  =======================config主界面=================
     root.config(
         background='#A2B5CD'
     )
+    #  =======================初始化r路侧界面================
     init_r_frame(root=root)
+    #  =======================启动主界面====================
     root.mainloop()
