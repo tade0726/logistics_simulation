299fbc481c9db2c8e02a94d54388d23a6389f388
teddyz <zp913913@gmail.com>
2017-08-05 01:21:48 +0800

    - some demo of digit generate

--- demo/generate_10_digit.py
+++ demo/generate_10_digit.py
@@ -1,4 +1,6 @@
 import math
+import time
+from functools import wraps
 
 
 def generator_digit_max(n):
@@ -7,11 +9,44 @@ def generator_digit_max(n):
     return generator_digit_max(n-1) + math.pow(10, n) * 9
 
 def generator_digit(n):
-    for i in range(int(generator_digit_max(n))):
+    for i in range(int(generator_digit_max(n-1))):
+        yield f"{i:0>{n}}"
+
+def generator_digit2(n):
+    for i in range(int('9' * n)):
         yield f"{i:0>{n}}"
 
 a = generator_digit(10)
+b = generator_digit2(10)
 
-for i, x in enumerate(a):
-    if i < 10:
-        print(x)
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        print('%r (%r, %r) %2.5f sec' % \
+              (func.__name__, args, kwargs, time.time() - t0) )
+        return result
+    return wrapper
+
+
+@timer
+def test1():
+    for i, x in enumerate(a):
+        if i < 10:
+            print(x)
+        else:
+            break
+
+@timer
+def test2():
+    for i, x in enumerate(b):
+        if i < 10:
+            print(x)
+        else:
+            break
+
+if __name__ == '__main__':
+    test1()
+    test2()
