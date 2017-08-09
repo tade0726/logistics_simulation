fc5415cbdd6838a65a5766e0d41123ec2cc611df
teddyz <zp913913@gmail.coom>
2017-08-09 15:11:01 +0800

    - sorter_type support nc

--- src/vehicles/items.py
+++ src/vehicles/items.py
@@ -49,7 +49,7 @@ class Package:
         self.ident_des_zno = self.attr["ident_des_zno"]
         self.dest_type = self.attr["dest_type"]
         self.parcel_type = self.attr["parcel_type"]
-        self.sorter_type = "reload" if self.parcel_type == "parcel" else "small_sort"
+        self.sorter_type = "small_sort" if self.parcel_type == "small" else "reload"
 
     # use in unload machine
     def set_path(self, package_start):
