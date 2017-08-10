7ec20d996e8dad4a1dbe57ddfcd1ce6a02197fed
teddyz <zp913913@gmail.com>
2017-08-10 15:41:14 +0800

    - add queue io data

--- src/utils/generate_data_queue_io.py
+++ src/utils/generate_data_queue_io.py
@@ -1,5 +1,12 @@
-
 # coding: utf-8
+
+"""
+Author: Ted
+
+build a queue table and insert to mysql
+
+"""
+
 
 
 import sys
@@ -12,14 +19,11 @@ from src.db import *
 
 
 def main():
-
-    table = load_from_mysql('i_queue_io')
-
+    table = load_from_mysql('i_que'
+                            'ue_io')
     c_ports_combos = list(product(['c9_1'], [f'c{i}_{j}' for i, j in product(range(13, 19), range(1, 29))]))
     data = pd.DataFrame(c_ports_combos, columns=['equipment_port_last', 'equipment_port_next'])
-
     queue_id = data.equipment_port_last.str.split('_').str.get(0) + '_' + data.equipment_port_next.str.split('_').str.get(0)
-
     data = data.assign(
                     equipment_type='c',
                     queue_id=queue_id,
@@ -31,7 +35,6 @@ def main():
                     normal_path=0,
                     inserted_on=np.datetime64('NaT'),
                     modified_on=np.datetime64('NaT'),)
-
     data = data[table.columns]
     data.to_sql(name='i_queue_io', con=RemoteMySQLConfig.engine, if_exists='append', index=0)
 
