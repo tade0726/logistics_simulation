20d7a33b4d22ad7ca278c1d5bad9020ad632ebbe
teddyz <zp913913@gmail.coom>
2017-08-10 16:19:59 +0800

    - try again deepcopy...

--- src/utils/generate_data_queue_io.py
+++ src/utils/generate_data_queue_io.py
@@ -18,8 +18,7 @@ from src.config import *
 
 
 def main():
-    table = load_from_mysql('i_que'
-                            'ue_io')
+    table = load_from_mysql('i_queue_io')
     c_ports_combos = list(product(['c9_1'], [f'c{i}_{j}' for i, j in product(range(13, 19), range(1, 29))]))
     data = pd.DataFrame(c_ports_combos, columns=['equipment_port_last', 'equipment_port_next'])
     queue_id = data.equipment_port_last.str.split('_').str.get(0) + '_' + data.equipment_port_next.str.split('_').str.get(0)
