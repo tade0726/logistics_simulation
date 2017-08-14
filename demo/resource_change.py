968a98348e27e8c70a89f7949b62bacdc475d78e
teddyz <zp913913@gmail.com>
2017-08-14 18:17:07 +0800

    - 测试修改资源的逻辑

--- demo/resource_change.py
+++ demo/resource_change.py
@@ -15,20 +15,20 @@ class Machine:
     def do_something(self, item):
         with self.resource.request() as req:
             yield req
-            yield self.env.timeout(1)
+            yield self.env.timeout(3)
             print(f"{self.env.now} - process: {self.machine_id} - deal with item: {item}")
 
     def run(self):
         while True:
             item = yield self.store.get()
             self.env.process(self.do_something(item))
-            yield env.timeout(3)
+            yield env.timeout(1)
 
 
 def change_resource_limit(env, resource_dict, time):
     yield env.timeout(time)
     for resource in resource_dict.values():
-        resource._capacity = 4
+        resource._capacity = 3
 
 
 if __name__ == '__main__':
@@ -43,7 +43,7 @@ if __name__ == '__main__':
         resource_dict[i] = simpy.Resource(env, capacity=1)
 
     machines = [ Machine(env, resource_dict, i, store) for i in range(6)]
-    env.process(change_resource_limit(env, resource_dict, 10))
+    env.process(change_resource_limit(env, resource_dict, 1000))
     for machine in machines:
         env.process(machine.run())
 
