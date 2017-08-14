b54dd20a119692bf270aa274a1c4af64e21f4d3a
teddyz <zp913913@gmail.com>
2017-08-14 11:29:51 +0800

    add data

--- src/utils/items.py
+++ src/utils/items.py
@@ -1,7 +1,32 @@
 from collections import namedtuple
+from sqlalchemy.types import String
+
 
 __all__ = ["TruckRecord", "PackageRecord", "PipelineRecord"]
 
 TruckRecord = namedtuple("truck_record", ["equipment_id", "truck_id", "time_stamp", "action", "store_size"])
 PackageRecord = namedtuple("package_record", ["equipment_id", "package_id", "time_stamp", "action"])
 PipelineRecord = namedtuple("pipeline_record", ["pipeline_id", "queue_id", "package_id", "time_stamp", "action"])
+
+
+class OutputTableColumnType:
+
+    truck_columns = dict(
+        equipment_id=String(length=32, ),
+        truck_id=String(length=32, ),
+        action=String(length=32, ),
+    )
+
+
+    package_columns = dict(
+        equipment_id=String(length=32, ),
+        package_id=String(length=32, ),
+        action=String(length=32, ),
+    )
+
+    pipeline_columns = dict(
+        pipeline_id=String(length=32, ),
+        queue_id=String(length=32, ),
+        package_id=String(length=32, ),
+        action=String(length=32, ),
+    )
