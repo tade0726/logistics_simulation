f73421b41e290604586be9e195778259e279dabb
teddyz <zp913913@gmail.coom>
2017-08-16 04:16:30 -0400

    修改插入数据的方式

--- src/utils/items.py
+++ src/utils/items.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-__all__ = ["TruckRecord", "PackageRecord", "PipelineRecord", "SmallPackageRecord"]
+__all__ = ["TruckRecord", "PackageRecord", "PipelineRecord",]
 
 TruckRecord = namedtuple("truck_record", ["equipment_id", "truck_id", "truck_type", "time_stamp", "action", "store_size"])
 PackageRecord = namedtuple("package_record", ["equipment_id", "parcel_id", "small_id", "parcel_type",
