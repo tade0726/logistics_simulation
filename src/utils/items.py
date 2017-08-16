518f2662d664f1f2c954b1cf318f12fd85574343
teddyz <zp913913@gmail.coom>
2017-08-16 04:25:32 -0400

    修改插入数据的方式

--- src/utils/items.py
+++ src/utils/items.py
@@ -1,9 +1,23 @@
 from collections import namedtuple
 
-__all__ = ["TruckRecord", "PackageRecord", "PipelineRecord",]
+__all__ = ["TruckRecord", "PackageRecord", "PipelineRecord",
+           "TruckRecordDict", "PackageRecordDict", "PipelineRecordDict"]
 
-TruckRecord = namedtuple("truck_record", ["equipment_id", "truck_id", "truck_type", "time_stamp", "action", "store_size"])
+TruckRecord = namedtuple("truck_record",
+                         ["equipment_id", "truck_id", "truck_type", "time_stamp", "action", "store_size"])
 PackageRecord = namedtuple("package_record", ["equipment_id", "parcel_id", "small_id", "parcel_type",
                                               "time_stamp", "action"])
 PipelineRecord = namedtuple("pipeline_record", ["pipeline_id", "queue_id", "parcel_id", "small_id", "parcel_type",
                                                 "time_stamp", "action"])
+
+
+class TruckRecordDict(dict):
+    pass
+
+
+class PackageRecordDict(dict):
+    pass
+
+
+class PipelineRecordDict(dict):
+    pass
