2e298636c084bb9d4c7efc1c8cd71af7ad93a930
teddyz <zp913913@gmail.coom>
2017-08-16 00:11:01 -0400

    添加小件包裹记录

--- src/utils/items.py
+++ src/utils/items.py
@@ -5,3 +5,4 @@ __all__ = ["TruckRecord", "PackageRecord", "PipelineRecord"]
 TruckRecord = namedtuple("truck_record", ["equipment_id", "truck_id", "time_stamp", "action", "store_size"])
 PackageRecord = namedtuple("package_record", ["equipment_id", "package_id", "time_stamp", "action"])
 PipelineRecord = namedtuple("pipeline_record", ["pipeline_id", "queue_id", "package_id", "time_stamp", "action"])
+SmallPackageRecord = namedtuple("small_package_record", ["equipment_id", "package_id", "small_id", "time_stamp", "action"])
