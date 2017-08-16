3105e383a3c5135654c8ad0712af1403a1e43757
teddyz <zp913913@gmail.coom>
2017-08-16 03:08:24 -0400

    添加输出数据字段

--- src/utils/items.py
+++ src/utils/items.py
@@ -1,8 +1,7 @@
 from collections import namedtuple
 
-__all__ = ["TruckRecord", "PackageRecord", "PipelineRecord"]
+__all__ = ["TruckRecord", "PackageRecord", "PipelineRecord", "SmallPackageRecord"]
 
 TruckRecord = namedtuple("truck_record", ["equipment_id", "truck_id", "time_stamp", "action", "store_size"])
-PackageRecord = namedtuple("package_record", ["equipment_id", "package_id", "time_stamp", "action"])
-PipelineRecord = namedtuple("pipeline_record", ["pipeline_id", "queue_id", "package_id", "time_stamp", "action"])
-SmallPackageRecord = namedtuple("small_package_record", ["equipment_id", "package_id", "small_id", "time_stamp", "action"])
+PackageRecord = namedtuple("package_record", ["equipment_id", "parcel_id", "small_id", "parcel_type", "time_stamp", "action"])
+PipelineRecord = namedtuple("pipeline_record", ["pipeline_id", "queue_id", "parcel_id", "small_id", "parcel_type", "time_stamp", "action"])
