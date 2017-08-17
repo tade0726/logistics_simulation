457c4a348f355209cb39cba0f91e501b5acf4913
teddyz <zp913913@gmail.com>
2017-08-17 10:41:40 +0800

    添加开关

--- src/machine/unload/unload.py
+++ src/machine/unload/unload.py
@@ -48,6 +48,10 @@ class Unload:
         self.truck_records = list()
         self.package_records = list()
 
+        # add machine switch
+        self.machine_switch = self.env.event()
+        self.machine_switch.succeed()
+
         self.resource_set = self._set_machine_resource()
 
     def _set_machine_resource(self):
@@ -65,6 +69,14 @@ class Unload:
             raise RuntimeError('unload machine',
                                self.machine_id,
                                'not initial equipment_resource_dict!')
+
+    def set_machine_open(self):
+        """设置为开机"""
+        self.machine_switch.succeed()
+
+    def set_machine_close(self):
+        """设置为关机"""
+        self.machine_switch = self.env.event()
 
     def process_package(self, process_idx, package: Package):
 
@@ -104,6 +116,8 @@ class Unload:
     def run(self):
 
         while True:
+            # 开关机的事件控制
+            yield self.machine_switch
             # filter out the match truck(LL/LA/AL/AA)
             truck = yield self.trucks_q.get(lambda x: x.truck_type in self.truck_types)
 
