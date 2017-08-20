39823ef11b1505e9c7b4d6a68267f733d307173e
teddyz <zp913913@gmail.com>
2017-08-20 14:34:19 +0800

    remove switch in machine class

--- src/machine/security/security.py
+++ src/machine/security/security.py
@@ -41,10 +41,6 @@ class Security:
         # 机器资源id与机器id映射字典
         self.equipment_resource_dict = equipment_resource_dict
 
-        # add machine switch
-        self.machine_switch = self.env.event()
-        self.machine_switch.succeed()
-
         # 初始化初分拣字典
         self.resource_set = self._set_machine_resource()
 
@@ -61,14 +57,6 @@ class Security:
             raise RuntimeError('cross machine',
                                self.machine_id,
                                'not initial equipment_resource_dict!')
-
-    def set_machine_open(self):
-        """设置为开机"""
-        self.machine_switch.succeed()
-
-    def set_machine_close(self):
-        """设置为关机"""
-        self.machine_switch = self.env.event()
 
     def processing(self, package: Package):
         # 请求资源（工人)
@@ -102,10 +90,6 @@ class Security:
 
     def run(self):
         while True:
-            # 开关机的事件控制
-            yield self.machine_switch
-            LOG.logger_font.debug(f"sim time: {self.env.now} - machine: {self.equipment_id} - do something")
-
             package = yield self.input_pip_line.get()
             # 有包裹就推送到资源模块
             yield self.env.process(self.processing(package))
