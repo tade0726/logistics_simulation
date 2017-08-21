5c93c7fb240359f80d7017b05849b0eb247d9fb2
teddyz <zp913913@gmail.com>
2017-08-22 02:45:16 +0800

    修改机器开关方式

--- src/machine/base.py
+++ src/machine/base.py
@@ -17,23 +17,6 @@ class BaseMachine:
         self.env = env
         self.switch_res = simpy.PriorityResource(self.env)
 
-    def _set_off_process(self, start, end):
-        """进程占用开关资源，模拟关机状态
-
-        start： 关机时间
-        end： 开机时间
-
-        """
-        yield self.env.timeout(start)
-        LOG.logger_font.debug(f"sim time: {self.env.now} - set off, until: {end}")
-        with self.switch_res.request(priority=-1) as req:
-            yield req
-            yield self.env.timeout(end - start)
-
-    def set_off(self, start, end):
-        """API"""
-        self.env.process(self._set_off_process(start, end))
-
     def check_switch(self):
         """开关机的事件控制"""
         t1 = self.env.now
