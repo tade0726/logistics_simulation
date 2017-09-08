bed2d1280970398ce7130bf899646239dc1d84b3
teddyz <zp913913@gmail.com>
2017-09-08 11:40:22 +0800

    兼容多个扎包时间

--- sim/machine/small_sort/small_reload.py
+++ sim/machine/small_sort/small_reload.py
@@ -59,7 +59,9 @@ class SmallReload(object):
         self.equipment_name = self.equipment_port.split('_')[0]
         self.parameters =  self.equipment_parameters[self.equipment_name]
         self.store_max = int(self.parameters['smallbag_wrap_condition'])
-        self.pack_time_list = [self.parameters[f"smallbag_wrap_time_{i}"] for i in range(1, 7)]
+
+        # 获取扎包时间的变量名
+        self.pack_time_list = sorted([v for k,v in self.parameters.items() if "smallbag_wrap_time" in k])
         self.input_pip_line = self.share_queue_dict[self.equipment_port]
 
     def _plan_pack_time(self):
